814c766f3b251ec8b998c1574b1d04cf8d528a26
nam.nhat <nam.nhat@manaty.net>
2019-12-24 14:42:05 +0700

    add button close and other buttons into the dialog

--- demo.js
+++ demo.js
@@ -1,7 +1,19 @@
 import { LitElement, html, css } from 'lit-element';
 import './mv-dialog.js';
+import "mv-button";
+import "mv-font-awesome";
 
 export class MvDialogDemo extends LitElement {
+  static get properties() {
+    return {
+      openA: { type: Boolean },
+      openB: { type: Boolean },
+      openC: { type: Boolean },
+      hasLeftButton: { type: String },
+      hasRightButton: { type: String },
+      hasCloseIcon: { type: Boolean }
+    };
+  }
 
   static get styles() {
     return css`
@@ -15,41 +27,130 @@ export class MvDialogDemo extends LitElement {
         width: 100%;
         margin-top: 200px;
       }
+      
+      p {
+        text-indent: 30px;
+        text-align: initial;
+      }
+      
+      .left-button {
+        position: absolute;
+        left: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      
+      .right-button {
+        position: absolute;
+        right: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      
+      .custom-size {
+        --mv-dialog-max-width: 500px;
+        --mv-dialog-body-width: 250px; // dialog height = 400px (250px + 150px height of header and footer)
+      }
    `;
-  }
-
-  static get properties() {
-    return {
-      dialogVisible: { type: Boolean },
-    };
   }
 
   constructor() {
     super();
-     this.dialogVisible = false;
+    this.leftButton = "Cancel";
+    this.rightButton = "Save";
+    this.openA = false;
+    this.openB = false;
+    this.openC = false;
+    this.hasCloseIcon = false;
   }
 
   render() {
     return html`
       <div class="container">
-        <button @click="${this.toggleDialog}"> Dialog</button>
-        <mv-dialog ?opened="${this.dialogVisible}" @dialog-cancel="${this.closeDialog}">
-          <h1 class="title ">Dialog</h1>
-          <p class="content">Dialogs inform users about a task and can contain critical information, require decisions, or involve multiple tasks.</p>
-          <div class="buttons">
-            <button class="accept" @click="${this.toggleDialog}">Save</button>
-            <button class="cancel" @click="${this.closeDialog}">Cancel</button>    
-          </div>
+        <mv-button @button-clicked="${this.openDialogA}" button-style="info">Dialog</mv-button>
+        <mv-button @button-clicked="${this.openDialogB}">Custom button</mv-button>
+        <mv-button @button-clicked="${this.openDialogC}" button-style="error">Custom size</mv-button>
+        <mv-dialog
+           ?open="${this.openA}"
+           @close-dialog="${this.closeDialogA}"
+           .leftButton="${this.leftButton}"
+           .rightButton="${this.rightButton}"
+           heading="Dialog"
+        >
+           <p>
+             A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+             Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+             Dialogs are purposefully interruptive, so they should be used sparingly.
+           </p>
+        </mv-dialog>
+        
+        <mv-dialog
+           ?open="${this.openB}"
+           @close-dialog="${this.closeDialogB}"
+           heading="Dialog custom button"
+        >
+           <p>
+             A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+             Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+             Dialogs are purposefully interruptive, so they should be used sparingly.
+           </p>
+           <mv-button class="left-button" slot="left-button" @button-clicked="${this.closeDialogB}">Cancel</mv-button>
+           <mv-button class="right-button" slot="right-button" @button-clicked="${this.closeDialogB}" button-style="info">Save</mv-button>
+        </mv-dialog>
+        
+        <mv-dialog
+           ?open="${this.openC}"
+           @close-dialog="${this.closeDialogC}"
+           heading="Dialog custom size"
+           class="custom-size"
+           .hasCloseIcon="${this.hasCloseIcon}"
+        >
+           <p>
+             A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+             Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+             Dialogs are purposefully interruptive, so they should be used sparingly.
+             A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+             Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+             Dialogs are purposefully interruptive, so they should be used sparingly.
+             A Dialog is a type of modal window that appears in front of app content to provide critical information or ask for a decision.
+             Dialogs disable all app functionality when they appear, and remain on screen until confirmed, dismissed, or a required action has been taken.
+             Dialogs are purposefully interruptive, so they should be used sparingly.
+           </p>
+           <mv-button 
+             class="left-button"
+             slot="left-button"
+             @button-clicked="${this.closeDialogC}"
+             button-style="error"
+           >
+             Cancel
+           </mv-button>
+           <mv-button class="right-button" slot="right-button" @button-clicked="${this.closeDialogC}">Save</mv-button>
         </mv-dialog>
       </div>`;
   }
 
-  toggleDialog(e) {
-    this.dialogVisible = !this.dialogVisible;
+  openDialogA() {
+    this.openA = true;
   }
 
-  closeDialog(e) {
-    this.dialogVisible = false;
+  closeDialogA() {
+    this.openA = false;
+  }
+
+  openDialogB() {
+    this.openB = true;
+  }
+
+  closeDialogB() {
+    this.openB = false;
+  }
+
+  openDialogC() {
+    this.openC = true;
+  }
+
+  closeDialogC() {
+    this.openC = false;
   }
 }
 
--- mv-dialog.js
+++ mv-dialog.js
@@ -1,26 +1,31 @@
 import { LitElement, html, css } from 'lit-element';
+import "mv-button";
+import "mv-font-awesome";
 
 export class MvDialog extends LitElement {
-  constructor() {
-    super();
-     this.opened = false;
-  }
-
   static get properties() {
     return {
-      opened: { type: Boolean },
+      open: { type: Boolean },
+      leftButton: { type: String },
+      rightButton: { type: String },
+      heading: { type: String },
+      hasCloseIcon: { type: Boolean }
     };
   }
 
   static get styles() {
     return css`
-      .wrapper {
+      :host {
+				font-family: var(--font-family, Arial);
+				font-size: var(--font-size-m, 10pt);				
+      }
+      
+      .mv-container-dialog {
         opacity: 0;
         transition: visibility 0s, opacity 0.25s ease-in;
       }
       
-      .overlay {
-        background: rgba(0, 0, 0, 0.8);
+      .overlay-dialog {
         height: 100%;
         position: fixed;
         top: 0;
@@ -28,13 +33,17 @@ export class MvDialog extends LitElement {
         bottom: 0;
         left: 0;
         width: 100%;
+        opacity: 0.5;
+        background-color: #000000;
       }
       
       .dialog {
-        background: #ffffff;
-        max-width: 600px;
-        padding: 1rem;
+        background: #FFFFFF;
+        max-width: var(--mv-dialog-max-width, 756px);
+        max-height: var(--mv-dialog-max-height, 528px);
         position: fixed;
+        box-shadow: 0 0 30px 0 rgba(0, 0, 0, 0.5);
+        border-radius: 5px;
       }
       
       .opened {
@@ -54,24 +63,116 @@ export class MvDialog extends LitElement {
       .closed {
         visibility: hidden;
       }
+      
+      mv-fa {
+        font-size: 20px;
+        color: #48C5B9;
+        position: absolute;
+        right: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+      }
+      
+      .header {
+         max-width: var(--mv-dialog-max-width, 756px);
+         height: 70px;
+         box-shadow: 0 5px 10px 0 rgba(7, 17, 26, 0.2); 
+         background: #FFFFFF;
+         border-radius: 5px 5px 0 0;
+         position: relative;
+      }
+      
+      .title {
+        font-size: 20px;
+        color: #80828C;
+        font-weight: 500;
+        position: absolute;
+        left: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: default;
+      }
+      
+      .footer {
+        max-width: var(--mv-dialog-max-width, 756px);
+        height: 80px;
+        box-shadow: 0 5px 10px 0 rgba(7, 17, 26, 0.2);
+        border-radius: 0 0 5px 5px;
+        border-top: 1px solid rgba(0, 0, 0, 0.12);
+        position: relative;
+      }
+      
+      .left-button {
+        position: absolute;
+        left: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      
+      .right-button {
+        position: absolute;
+        right: 30px;
+        top: 50%;
+        transform: translateY(-50%);
+      }
+      
+      .body {
+        overflow-y: auto;
+        max-width: var(--mv-dialog-max-width, 756px);
+        height: var(--mv-dialog-body-width, 378px);
+        position: relative;
+        padding: 0 30px 0 30px;
+      } 
    `;
   }
 
+  constructor() {
+    super();
+    this.open = false;
+    this.leftButton = null;
+    this.rightButton = null;
+    this.heading = "Dialog";
+    this.hasCloseIcon = true;
+  }
+
   render() {
-    const dialogClass = this.opened ? 'opened' : 'closed';
+    const dialogClass = this.open ? 'opened' : 'closed';
     return html`
-      <div class="wrapper ${dialogClass}">
-        <div class="overlay" @click="${this.close}"></div>
-          <div class="dialog" role="dialog" aria-labelledby="title" aria-describedby="content">
-            <slot></slot>
+      <div class="mv-container-dialog ${dialogClass}">
+        <div class="overlay-dialog" @click="${this.handleClose}"></div>
+        <div class="dialog" role="dialog" aria-labelledby="title" aria-describedby="content">
+          <div class="header">
+              ${this.hasCloseIcon
+                ? html`<mv-fa icon="window-close" @click="${this.handleClose}"></mv-fa>`
+                : html``}  
+              <span class="title">${this.heading}</span>
+          </div>
+          
+          <div class="body">
+              <slot></slot>
+          </div>
+          
+          <div class="footer">
+             ${this.leftButton
+              ? html`<mv-button class="left-button" button-style="info" @button-clicked="${this.handleClose}">
+                        ${this.leftButton}
+                     </mv-button>`
+              : html``}  
+             
+             ${this.rightButton
+              ? html`<mv-button class="right-button" @button-clicked="${this.handleClose}">${this.rightButton}</mv-button>`
+              : html``}
+             <slot name="left-button"></slot>
+             <slot name="right-button"></slot>
           </div>
         </div>
       </div>
    `;
   }
 
-  close() {
-    this.dispatchEvent(new CustomEvent('dialog-cancel'))
+  handleClose() {
+    this.dispatchEvent(new CustomEvent('close-dialog'))
   }
 }
 
